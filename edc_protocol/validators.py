abd47201bd94cb2e304b65c0d82752da7bdf44d1
erikvw <ew2789@gmail.com>
2016-11-21 12:16:08 +0200

    assume all datetimes timezone aware

--- edc_protocol/validators.py
+++ edc_protocol/validators.py
@@ -1,42 +1,35 @@
 import pytz
 
-from datetime import datetime
-
 from django.apps import apps as django_apps
-from django.conf import settings
-from django.utils.timezone import make_naive, make_aware
+from django.utils import timezone
 from django.core.exceptions import ValidationError
 
 app_config = django_apps.get_app_config('edc_protocol')
-tz = pytz.timezone(settings.TIME_ZONE)
-
-
-def my_make_naive(value, tz):
-    try:
-        value = make_naive(value, tz)
-    except ValueError:
-        pass
-    return value
-
-
-def my_make_aware(value, tz):
-    try:
-        value = make_aware(value, tz)
-    except ValueError:
-        pass
-    return value
+tz = pytz.timezone('UTC')
 
 
 def date_not_before_study_start(value):
-    value_datetime = my_make_aware(datetime(value.year, value.month, value.day, 0, 0), tz)
-    if value_datetime < app_config.study_open_datetime:
-        raise ValidationError(
-            'Invalid date. Study opened on {}. Got {}. See edc_protocol.AppConfig'.format(
-                app_config.study_open_datetime, value_datetime))
+    if value:
+        try:
+            value.date()
+            raise TypeError('Expected date but got a datetime. Got {}'.format(value.strftime('%Y-%m-%d %H:%M')))
+        except AttributeError:
+            pass
+        study_open_date = timezone.localtime(app_config.study_open_datetime).date()
+        if value < study_open_date:
+            raise ValidationError(
+                'Invalid date. Study opened on {}. Got {}. See edc_protocol.AppConfig'.format(
+                    study_open_date, value))
 
 
-def datetime_not_before_study_start(value):
-    value = my_make_aware(value, tz)
-    if value < app_config.study_open_datetime:
-        raise ValidationError(
-            'Invalid date. Study opened on {}. Got {}.'.format(app_config.study_open_datetime, value))
+def datetime_not_before_study_start(value_datetime):
+    if value_datetime:
+        try:
+            value_datetime.date()
+        except AttributeError:
+            raise TypeError('Expected datetime but got a date. Got {}'.format(value_datetime.strftime('%Y-%m-%d')))
+        if value_datetime < app_config.study_open_datetime:
+            raise ValidationError(
+                'Invalid date/time. Study opened on {}. Got {}.'.format(
+                    timezone.localtime(app_config.study_open_datetime).strftime('%Y-%m-%d %H:%M'),
+                    timezone.localtime(value_datetime).strftime('%Y-%m-%d %H:%M')))
