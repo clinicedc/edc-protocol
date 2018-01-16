e3d06f6890eb8fe7ec0bc4caefc884e286c7f281
erikvw <ew2789@gmail.com>
2018-01-16 19:40:54 +0200

    fix urls for tests

--- edc_protocol/urls.py
+++ edc_protocol/urls.py
@@ -1,23 +1,17 @@
 from django.conf import settings
-from django.contrib import admin
 from django.urls.conf import path
-from edc_base.views import AdministrationView, LoginView, LogoutView
+from edc_base.views import AdministrationView
 
 from .views import HomeView
 
 app_name = 'edc_protocol'
 
+urlpatterns = [
+    path('', HomeView.as_view(), name='home_url'),
+]
+
+
 if settings.APP_NAME == 'edc_protocol':
     urlpatterns = [
         path('administration/', AdministrationView.as_view(),
-             name='administration_url'),
-        path('login', LoginView.as_view(), name='login_url'),
-        path('logout', LogoutView.as_view(
-            pattern_name='login_url'), name='logout_url')]
-else:
-    urlpatterns = []
-
-urlpatterns += [
-    path('admin/', admin.site.urls),
-    path('', HomeView.as_view(), name='home_url'),
-]
+             name='administration_url')] + urlpatterns
