b2236517251d1d67a691024948125de301ba25a9
erikvw <ew2789@gmail.com>
2018-07-21 09:19:33 +0200

    update setup files, flake8

--- setup.py
+++ setup.py
@@ -6,19 +6,22 @@ from setuptools import find_packages
 with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
     README = readme.read()
 
+with open(os.path.join(os.path.dirname(__file__), 'VERSION')) as f:
+    VERSION = f.read()
+
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='edc-protocol',
-    version='0.1.5',
+    version=VERSION,
     author=u'Erik van Widenfelt',
     author_email='ew2789@gmail.com',
     packages=find_packages(),
     include_package_data=True,
     url='https://github.com/clinicedc/edc-protocol',
     license='GPL license, see LICENSE',
-    description='Protocol specific variables.',
+    description='Protocol specific variables for clinicedc/edc.',
     long_description=README,
     zip_safe=False,
     keywords='django research protocol EDC',
@@ -29,10 +32,11 @@ setup(
         'Environment :: Web Environment',
         'Framework :: Django',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 3.6',
-        'Topic :: Security :: Cryptography',
+        'Programming Language :: Python :: 3.7',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
     ],
