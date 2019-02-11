00862bb1407867b481af93b7144d6d9754f19950
erikvw <ew2789@gmail.com>
2019-02-10 23:29:49 -0600

    format date in message according to settings

--- edc_protocol/validators.py
+++ edc_protocol/validators.py
@@ -1,11 +1,11 @@
 import arrow
 
 from dateutil.tz import gettz
-
 from django.apps import apps as django_apps
 from django.conf import settings
 from django.core.exceptions import ValidationError
 from django.utils import timezone
+from edc_base.utils import formatted_datetime
 
 
 def date_not_before_study_start(value):
@@ -14,12 +14,12 @@ def date_not_before_study_start(value):
         tzinfo = gettz(settings.TIME_ZONE)
         value_utc = arrow.Arrow.fromdate(value, tzinfo).to('utc').datetime
         if value_utc < app_config.study_open_datetime:
+            opened = formatted_datetime(timezone.localtime(
+                app_config.study_open_datetime))
+            got = formatted_datetime(timezone.localtime(value_utc))
             raise ValidationError(
-                'Invalid date. Study opened on {}. Got {}. '
-                'See edc_protocol.AppConfig'.format(
-                    timezone.localtime(
-                        app_config.study_open_datetime).strftime('%Y-%m-%d'),
-                    timezone.localtime(value_utc).strftime('%Y-%m-%d')))
+                f'Invalid date. Study opened on {opened}. Got {got}. '
+                f'See edc_protocol.AppConfig.')
 
 
 def datetime_not_before_study_start(value_datetime):
@@ -28,8 +28,9 @@ def datetime_not_before_study_start(value_datetime):
         value_utc = arrow.Arrow.fromdatetime(
             value_datetime, value_datetime.tzinfo).to('utc').datetime
         if value_utc < app_config.study_open_datetime:
+            opened = formatted_datetime(
+                timezone.localtime(app_config.study_open_datetime))
+            got = formatted_datetime(timezone.localtime(value_utc))
             raise ValidationError(
-                'Invalid date/time. Study opened on {}. Got {}.'.format(
-                    timezone.localtime(app_config.study_open_datetime).strftime(
-                        '%Y-%m-%d %H:%M'),
-                    timezone.localtime(value_utc).strftime('%Y-%m-%d %H:%M')))
+                f'Invalid date/time. Study opened on {opened}. Got {got}.'
+                f"See edc_protocol.AppConfig.")
