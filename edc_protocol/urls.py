93dade668f2b038b4462d3a223566025be061a6b
erikvw <ew2789@gmail.com>
2019-03-04 11:26:30 -0600

    update imports for edc-base

--- edc_protocol/urls.py
+++ edc_protocol/urls.py
@@ -3,15 +3,14 @@ from django.urls.conf import path
 
 from .views import HomeView
 
-app_name = 'edc_protocol'
+app_name = "edc_protocol"
 
-urlpatterns = [
-    path('', HomeView.as_view(), name='home_url'),
-]
+urlpatterns = [path("", HomeView.as_view(), name="home_url")]
 
 
-if settings.APP_NAME == 'edc_protocol':
+if settings.APP_NAME == "edc_protocol":
     from edc_dashboard.views import AdministrationView
+
     urlpatterns = [
-        path('administration/', AdministrationView.as_view(),
-             name='administration_url')] + urlpatterns
+        path("administration/", AdministrationView.as_view(), name="administration_url")
+    ] + urlpatterns
