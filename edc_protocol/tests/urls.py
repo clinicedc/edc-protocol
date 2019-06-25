ac62ef373896a75c8d1215055a0cfbfab266d661
erikvw <ew2789@gmail.com>
2019-06-24 21:35:13 -0500

    include in admin section

--- edc_protocol/tests/urls.py
+++ edc_protocol/tests/urls.py
@@ -3,6 +3,5 @@ from edc_dashboard.views import AdministrationView
 
 urlpatterns = [
     path("protocol/", include("edc_protocol.urls")),
-    path("administration/", AdministrationView.as_view(),
-         name="administration_url"),
+    path("administration/", AdministrationView.as_view(), name="administration_url"),
 ]
