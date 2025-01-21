b8ec646476f944e84e8f3132cc89c0134eea878b
erikvw <ew2789@gmail.com>
2025-01-20 20:30:08 -0600

    remove refs to bootstrap3 template folder

--- edc_protocol/views.py
+++ edc_protocol/views.py
@@ -1,7 +1,6 @@
 from typing import Any
 
 from django.views.generic.base import TemplateView
-from edc_dashboard.utils import get_bootstrap_version
 from edc_dashboard.view_mixins import EdcViewMixin
 from edc_navbar import NavbarViewMixin
 
@@ -9,7 +8,7 @@ from .research_protocol_config import ResearchProtocolConfig
 
 
 class HomeView(EdcViewMixin, NavbarViewMixin, TemplateView):
-    template_name = f"edc_protocol/bootstrap{get_bootstrap_version()}/home.html"
+    template_name = "edc_protocol/home.html"
     navbar_name = "edc_protocol"
     navbar_selected_item = "protocol"
 
